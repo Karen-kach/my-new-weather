72d645b94796b4fc2ebc05ea66356f1111dbccd0
Karen-kach <59180761+Karen-kach@users.noreply.github.com>
2020-02-05 23:01:42 +0400

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const weatherURL="https://api.openweathermap.org/data/2.5/forecast?q=Yerevan&lang=us&units=metric&APPID=a9a3a62789de80865407c0452e9d1c27"
+
+
+const  App_weather = () => {
+    const [days, setDays] = useState('')
+
+    useEffect(() => {
+        fetch(weatherURL)
+        .then(resp => resp.json())
+        .then(data => {
+            const dailyData = data.list.filter(reading => reading.dt_txt.includes('12:00:00'))
+            setDays(dailyData)
+            
+        })
+    })
+    return (
+        <div className='weather'>
+            <div>
+                <h1>Forecast 5 Day</h1>
+                <h5>Yerevan</h5>
+            </div>
+            <div className='weather_day'>
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+
+
+export default App_weather
